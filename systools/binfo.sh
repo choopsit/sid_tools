170e43e14b32131746be4b0c8ce90329ba1a835f
choopsit <choopsbd@gmail.com>
2026-07-07 00:09:28 +0200

    review terminal and DE output

--- systools/binfo.sh
+++ systools/binfo.sh
@@ -28,6 +28,25 @@ usage() {
     exit "$errcode"
 }
 
+trim() {
+    set -f
+    # shellcheck disable=2048,2086
+    set -- $*
+    printf '%s\n' "${*//[[:space:]]/}"
+    set +f
+}
+
+get_ppid() {
+    ppid="$(grep -i -F "PPid:" "/proc/${1:-$PPID}/status")"
+    ppid="$(trim "${ppid/PPid:}")"
+    printf "%s" "$ppid"
+}
+
+get_process_name() {
+    name="$(< "/proc/${1:-$PPID}/comm")"
+    printf "%s" "$name"
+}
+
 
 [[ $1 =~ ^-(h|-help)$ ]] && usage 0
 
@@ -60,25 +79,55 @@ else
     my_shell+=" $shell_version"
 fi
 
-my_terminal=""
-if [[ -f /etc/alternatives/x-terminal-emulator ]]; then
-    sept="\t\t    "
-    #sept="\t\t\t"
-    if [[ ${#my_shell} -lt 7 ]]; then
-        sept="\t\t\t    "
-        #sept="\t\t\t\t"
-    elif [[ ${#my_shell} -gt 14 ]];then
-        sept="\t    "
-        #sept="\t\t"
-    fi
-    if (grep -q terminator /etc/alternatives/x-terminal-emulator); then
-        term="terminator"
-    else
-        term="$(awk -F"'" '/exec/ {print $2}' /etc/alternatives/x-terminal-emulator)"
-    fi
-    term+=" $(dpkg-query -W "$term" | awk '{print $2}')"
-    my_terminal="$sept${CYN}Terminal$DEF: $term"
+# Check $PPID for terminal emulator.
+while [[ -z "$term" ]]; do
+    parent="$(get_ppid "$parent")"
+    [[ -z "$parent" ]] && break
+    name="$(get_process_name "$parent")"
+
+    case ${name// } in
+        "${SHELL/*\/}"|*"sh"|"screen"|"su"*|"newgrp") ;;
+
+        "login"*|*"Login"*|"init"|"(init)")
+            term="$(tty)"
+            ;;
+
+            "ruby"|"1"|"tmux"*|"systemd"|"sshd"*|"python"*|\
+                "USER"*"PID"*|"kdeinit"*|"launchd"*|"bwrap")
+            break
+            ;;
+
+        "gnome-terminal-") term="gnome-terminal" ;;
+        "urxvtd")          term="urxvt" ;;
+        *"nvim")           term="Neovim Terminal" ;;
+        *"NeoVimServer"*)  term="VimR Terminal" ;;
+
+        *)
+            # Fix issues with long process names on Linux.
+            [[ $os == Linux ]] && term=$(realpath "/proc/$parent/exe")
+
+            term="${name##*/}"
+
+                # Fix wrapper names in Nix.
+                [[ $term == .*-wrapped ]] && {
+                    term="${term#.}"
+                                    term="${term%-wrapped}"
+                }
+            ;;
+    esac
+done
+
+sept="\t\t    "
+#sept="\t\t\t"
+if [[ ${#my_shell} -lt 7 ]]; then
+    sept="\t\t\t    "
+    #sept="\t\t\t\t"
+elif [[ ${#my_shell} -gt 14 ]];then
+    sept="\t    "
+    #sept="\t\t"
 fi
+my_terminal="$sept${CYN}Terminal$DEF: $term"
+my_terminal+=" $(dpkg-query -W "$term" | awk '{print $2}')"
 
 my_packages="${CYN}Packages$DEF: $(dpkg -l | grep '^ii' | wc -l)"
 
@@ -87,12 +136,14 @@ if { [[ $XDG_CURRENT_DESKTOP ]] || [[ $DESKTOP_SESSION ]] ; }; then
     if [[ $XDG_CURRENT_DESKTOP ]]; then
         if [[ $XDG_CURRENT_DESKTOP == XFCE ]]; then
             de="$DESKTOP_SESSION $(dpkg-query -W xfce4 | awk '{print $2}')"
+        elif [[ $XDG_CURRENT_DESKTOP == GNOME ]]; then
+            de="${XDG_CURRENT_DESKTOP,,} $(dpkg-query -W gnome | awk '{print $2}')"
         else
             de="$XDG_CURRENT_DESKTOP"
         fi
     else
-            de="$DESKTOP_SESSION"
-        fi
+        de="$DESKTOP_SESSION"
+    fi
     my_desktop="\t    ${CYN}DE$DEF: $de"
     #my_desktop="\t\t${CYN}DE$DEF: $de"
 fi
